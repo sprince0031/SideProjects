7de0184966a2de2361eb06b56d5c9fb88735cb80
Siddharth Prince <siddharthprince31@gmail.com>
2016-12-03 18:59:58 +0530

    Update flames.py
    Commenting a few lines in the code to make it better understandable. :)

--- Python/FLAMES/flames.py
+++ Python/FLAMES/flames.py
@@ -17,7 +17,7 @@ name2 = raw_input("Enter your crush's full name: ").upper()
 namelist1 = name1.split();
 namelist2 = name2.split();
 
-#Part where all the common alphabets arec cancelled.
+#Part where all the common alphabets are cancelled.
 print "\nStep 1: Cancelling all common letters..."
 def cancel():
 	len1, len2 = reset(namelist1, namelist2);	
