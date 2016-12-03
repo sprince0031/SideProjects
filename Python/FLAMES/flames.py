e05b66aa82d452b89dd3bf80bb6b4a4dee562ee7
Siddharth Prince <siddharthprince31@gmail.com>
2016-12-03 16:09:14 +0530

    Update flames.py
    Added functionality in a scenario where all alphabets get canceled.

--- Python/FLAMES/flames.py
+++ Python/FLAMES/flames.py
@@ -8,18 +8,16 @@ def reset(namelistx, namelisty):
 	len1, len2 = [], [];
 	for name in namelistx:
 		len1.append(len(name));
-	#print len1;
 	for name in namelisty:
 		len2.append(len(name));
-	#print len2;
 	return len1, len2;
 
 name1 = raw_input("Enter your full name: ").upper()
 name2 = raw_input("Enter your crush's full name: ").upper()
 namelist1 = name1.split();
 namelist2 = name2.split();
-#print namelist1;
-#print namelist2;
+
+#Part where the all the common alphabets arec cancelled.
 print "\nStep 1: Cancelling all common letters..."
 def cancel():
 	len1, len2 = reset(namelist1, namelist2);	
@@ -45,17 +43,17 @@ def cancel():
 							if y != 0 and b != 0 :
 								namelist1[x] = namelist1[x][:y] + namelist1[x][y+1:];
 								namelist2[a] = namelist2[a][:b] + namelist2[a][b+1:];
-							print namelist1;
-							print namelist2;
-							cancel();
+							print namelist1
+							print namelist2
+							cancel() #No support for labels in python unlike in C or C++. Hence a recursive function is used here.
 					except:
 						break;
-					b=b+1;
-				a=a+1;
-				j=j+1;
-			y=y+1;
-		x=x+1;
-		i=i+1;
+					b=b+1
+				a=a+1
+				j=j+1
+			y=y+1
+		x=x+1
+		i=i+1
 cancel()
 
 finalName1, finalName2 = "","";
@@ -66,91 +64,99 @@ for word in namelist2:
 	finalName2 += word
 finalName = finalName1 + finalName2
 totLen = len(finalName)
-print "\nThe remaining string is " + finalName + " and the it's length is",str(totLen) + '.'
 
-print "\nStep 2: Cancelling letters in 'FLAMES'..."
-def Flames(totLenx):
-	flames = "FLAMES"
-	x = 6
-	while len(flames) > 1:
-		if totLenx % x == 0:
-			pos = x-1
-			if pos == 0:
-				flames = flames[1:]
-				print flames
-			if pos != 0:
-				flames = flames[pos+1:] + flames[:pos]
-				print flames 
-		else:
-			pos = (totLenx % x)-1
-			if pos == 0:
-				flames = flames[1:]
-				print flames
-			if pos != 0:
-				flames = flames[pos+1:] + flames[:pos]
-				print flames 
-		x-=1
-	return flames
+#Part where the flame is determined.
+if totLen > 0:
+	print "\nThe remaining string is " + finalName + " and the it's length is",str(totLen) + '.'
+	print "\nStep 2: Cancelling letters in 'FLAMES'..."
+	def Flames(totLenx):
+		flames = "FLAMES"
+		x = 6
+		while len(flames) > 1:
+			if totLenx % x == 0:
+				pos = x-1
+				if pos == 0:
+					flames = flames[1:]
+					print flames
+				if pos != 0:
+					flames = flames[pos+1:] + flames[:pos]
+					print flames 
+			else:
+				pos = (totLenx % x)-1
+				if pos == 0:
+					flames = flames[1:]
+					print flames
+				if pos != 0:
+					flames = flames[pos+1:] + flames[:pos]
+					print flames 
+			x-=1
+		return flames
 
-if totLen > 6:	
-	finalFlames = Flames(totLen)
+	if totLen > 6:	
+		finalFlames = Flames(totLen)
 
+	else:
+		if totLen == 6:
+			print "FLAME"
+			print "LAME"
+			print "MEL"
+			print "ME"
+			print "M"
+			finalFlames = 'M'
+		elif totLen == 5:
+			print "SFLAM"
+			print "SFLA"
+			print "FLA"
+			print "AF"
+			print "F"
+			finalFlames = 'F'
+		elif totLen == 4:
+			print "ESFLA"
+			print "ESFA"
+			print "AES"
+			print "ES"
+			print "E"
+			finalFlames = 'E'
+		elif totLen == 3:
+			print "MESFL"
+			print "FLME"
+			print "EFL"
+			print "EF"
+			print "F"
+			finalFlames = 'F'
+		elif totLen == 2:
+			print "AMESF"
+			print "ESFA"
+			print "FAE"
+			print "EF"
+			print "E"
+			finalFlames = 'E'
+		elif totLen == 1:
+			print "LAMES"
+			print "AMES"
+			print "MES"
+			print "ES"
+			print "S"
+			finalFlames = 'S'
 else:
-	if totLen == 6:
-		print "FLAME"
-		print "LAME"
-		print "MEL"
-		print "ME"
-		print "M"
-		finalFlames = 'M'
-	elif totLen == 5:
-		print "SFLAM"
-		print "SFLA"
-		print "FLA"
-		print "AF"
-		print "F"
-		finalFlames = 'F'
-	elif totLen == 4:
-		print "ESFLA"
-		print "ESFA"
-		print "AES"
-		print "ES"
-		print "E"
-		finalFlames = 'E'
-	elif totLen == 3:
-		print "MESFL"
-		print "FLME"
-		print "EFL"
-		print "EF"
-		print "F"
-		finalFlames = 'F'
-	elif totLen == 2:
-		print "AMESF"
-		print "ESFA"
-		print "FAE"
-		print "EF"
-		print "E"
-		finalFlames = 'E'
-	elif totLen == 1:
-		print "LAMES"
-		print "AMES"
-		print "MES"
-		print "ES"
-		print "S"
-		finalFlames = 'S'
+	print "\nEither you're head over heels in love with yourself or both your names are anagrams of each other... :O"
 
 #Final result
-if finalFlames == 'F':
-	print "Awesome! The FLAMES calculator predicts that you two will be best friends :D"
-elif finalFlames == 'L':
-	print "Congratulations! The FLAMES calculator predicts you two will fall in love <3"
-elif finalFlames == 'A':
-	print "Awww... The FLAMES calculator predicts you two will have mutual affection towards each other :)"
-elif finalFlames == 'M':
-	print "*Here comes the bride... All dressed in white...* The FLAMES calculator wishes you two a happy married life!!! O*"
-elif finalFlames == 'E':
-	print "Oops! Sorry, but the FLAMES calculator predicts you two will be enemies! :("
-else:
-	print "Unmaigal sela times kasaka thaan saiyum BROTHER... ;)"
+try:
+	if finalFlames == 'F':
+		print "Awesome! The FLAMES calculator predicts that you two will be best friends :D"
+	elif finalFlames == 'L':
+		print "Congratulations! The FLAMES calculator predicts you two will fall in love <3"
+	elif finalFlames == 'A':
+		print "Awww... The FLAMES calculator predicts you two will have mutual affection towards each other :)"
+	elif finalFlames == 'M':
+		print "*Here comes the bride... All dressed in white...* The FLAMES calculator wishes you two a happy married life!!! O*"
+	elif finalFlames == 'E':
+		print "Oops! Sorry, but the FLAMES calculator predicts you two will be enemies! :("
+	else:
+		print "Tough luck... looks like someone is getting bro-zoned :P"
+except:
+	pass;
+
 print "\nProgram by sprince0031"
 print "***END OF PROGRAM***"
